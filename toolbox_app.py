8948a30bf3ef561b56de9075aba546e575bda49e
rumiwarmi <mayra.mezarina@gmail.com>
2023-09-20 15:28:30 +0200

    Correction bugs

--- toolbox_app.py
+++ toolbox_app.py
@@ -92,7 +92,6 @@ def inject_conf_var():
 # ROUTES
 #-----------------------------------------------------------------
 @app.route('/')
-@app.route('/index')
 def index():
 	return render_template('index.html')
 
